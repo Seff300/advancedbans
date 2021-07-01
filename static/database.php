f983af790246fdfcb17c6b3066b34f1a8745c124
Seff300 <37154442+Seff300@users.noreply.github.com>
2021-07-01 13:38:19 +0300

    Delete database.php

--- static/database.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-define("DATABASE_HOST", "45.86.168.220");
-define("DATABASE_USER", "u638_d65JJXlkom");
-define("DATABASE_PASSWORD", "=jDi@g0bYqbkd8X09w^1X@ld");
-define("DATABASE_DATABASE", "s638_karistusedv4");
